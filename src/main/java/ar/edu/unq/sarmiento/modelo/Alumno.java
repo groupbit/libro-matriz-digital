cdd9e4d9784dfa0c67f39a0bd55c31a69d656a04
Natalia <natalia070881@hotmail.com>
2018-10-18 23:36:03 -0300

    agregado package del modelo y clase alumno en modelo

---
+++ src/main/java/ar/edu/unq/sarmiento/modelo/Alumno.java
@@ -0,0 +1,16 @@
+package ar.edu.unq.sarmiento.modelo;
+
+
+public class Alumno {
+	
+	private String nombre;
+
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+}
