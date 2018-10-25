d613000d16f9d8142f087979dbd17ec3fdf85579
Natalia <natalia070881@hotmail.com>
2018-10-25 14:56:34 -0300

    Creacion de clases Carrera y Materia como entidad, agregado de Persistible a clase Alumno

--- src/main/java/ar/edu/unq/sarmiento/modelo/Alumno.java
+++ src/main/java/ar/edu/unq/sarmiento/modelo/Alumno.java
@@ -2,10 +2,21 @@ package ar.edu.unq.sarmiento.modelo;
 
 import javax.persistence.Entity;
 
-public class Alumno {
+@Entity
+public class Alumno extends Persistible{
+	
+	private static final long serialVersionUID = 4514021643174653681L;
 	
 	private String nombre;
 
+	
+	
+	
+	
+	
+	
+	
+	
 	public String getNombre() {
 		return nombre;
 	}
---
+++ src/main/java/ar/edu/unq/sarmiento/modelo/Materia.java
@@ -0,0 +1,12 @@
+package ar.edu.unq.sarmiento.modelo;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Materia extends Persistible{
+
+	private static final long serialVersionUID = -5190167380605219656L;
+	
+	
+
+}
