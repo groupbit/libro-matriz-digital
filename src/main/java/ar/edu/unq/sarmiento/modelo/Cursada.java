776549501afdddb57f1bb2ecfa71ce8fd6d3dca8
Juan Cruz Santamaria <jcruzsantamaria@hotmail.com>
2018-10-24 21:13:31 -0300

    cursada, docente, examen creados

---
+++ src/main/java/ar/edu/unq/sarmiento/modelo/Cursada.java
@@ -0,0 +1,13 @@
+package ar.edu.unq.sarmiento.modelo;
+
+public class Cursada {
+	private long id;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+}
---
+++ src/main/java/ar/edu/unq/sarmiento/modelo/Examen.java
@@ -0,0 +1,14 @@
+package ar.edu.unq.sarmiento.modelo;
+
+public class Examen {
+
+	private long id;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+}
