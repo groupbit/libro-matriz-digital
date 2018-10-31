fa9984c4af7288a88fa489d46a517425defcde32
Juan Cruz Santamaria <jcruzsantamaria@hotmail.com>
2018-10-31 18:48:29 -0300

    commit

--- src/main/java/ar/edu/unq/sarmiento/modelo/Cursada.java
+++ src/main/java/ar/edu/unq/sarmiento/modelo/Cursada.java
@@ -1,13 +1,5 @@
 package ar.edu.unq.sarmiento.modelo;
 
-public class Cursada {
-	private long id;
-
-	public long getId() {
-		return id;
-	}
-
-	public void setId(long id) {
-		this.id = id;
-	}
+public class Cursada extends Persistible {
+	
 }
--- src/main/java/ar/edu/unq/sarmiento/modelo/Examen.java
+++ src/main/java/ar/edu/unq/sarmiento/modelo/Examen.java
@@ -1,14 +1,5 @@
 package ar.edu.unq.sarmiento.modelo;
 
-public class Examen {
+public class Examen extends Persistible{
 
-	private long id;
-
-	public long getId() {
-		return id;
-	}
-
-	public void setId(long id) {
-		this.id = id;
-	}
 }
