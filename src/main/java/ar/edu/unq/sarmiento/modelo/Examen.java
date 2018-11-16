e50a4d9b37bf29c450fb315c81196216a904f5e9
Juan Cruz Santamaria <jcruzsantamaria@hotmail.com>
2018-11-15 22:08:38 -0300

    datos en examen completados

--- src/main/java/ar/edu/unq/sarmiento/modelo/Examen.java
+++ src/main/java/ar/edu/unq/sarmiento/modelo/Examen.java
@@ -2,18 +2,32 @@ package ar.edu.unq.sarmiento.modelo;
 
 import javax.persistence.Entity;
 
+import org.hibernate.annotations.Type;
+
 @Entity
 public class Examen extends Persistible{
 
-	private String examenFinal;
-
-	public String getExamenFinal() {
-		return examenFinal;
+	private TipoDeExamen tipoDeExamen;
+	private float calificacion;
+	@Type(type= "yes_no")
+	private boolean estuvoausente;
+	public TipoDeExamen getTipoDeExamen() {
+		return tipoDeExamen;
+	}
+	public void setTipoDeExamen(TipoDeExamen tipoDeExamen) {
+		this.tipoDeExamen = tipoDeExamen;
+	}
+	public float getCalificacion() {
+		return calificacion;
+	}
+	public void setCalificacion(float calificacion) {
+		this.calificacion = calificacion;
+	}
+	public boolean isEstuvoausente() {
+		return estuvoausente;
+	}
+	public void setEstuvoausente(boolean estuvoausente) {
+		this.estuvoausente = estuvoausente;
 	}
 
-	public void setExamenFinal(String examenFinal) {
-		this.examenFinal = examenFinal;
-	}
-	
-	
 }
