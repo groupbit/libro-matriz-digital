5a1e58f3b76767a8fef5dd5ed614fdfe8e761297
nataliaMarzec <natimarzec708@gmail.com>
2018-11-26 22:10:45 -0300

    creacion de metodo getAlumno en AlumnoHome

--- src/main/java/ar/edu/unq/sarmiento/hibernate/AlumnoHome.java
+++ src/main/java/ar/edu/unq/sarmiento/hibernate/AlumnoHome.java
@@ -7,4 +7,10 @@ import ar.edu.unq.sarmiento.modelo.Alumno;
 @Component
 public class AlumnoHome extends Home<Alumno>{
 	private static final long serialVersionUID = 1L;
+	
+	
+	public Alumno getAlumno(Alumno alumno){
+		return getSession().createQuery("FROM Alumno",Alumno.class).getSingleResult();
+	}
+	
 }
