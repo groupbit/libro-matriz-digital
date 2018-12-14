51f534a72631f173c9e780bb2e1f02171d0d1195
Federico Aloi <federico.aloi@gmail.com>
2018-12-14 16:03:12 -0300

    Borrados comentarios antes del serialId

--- src/main/java/ar/edu/unq/sarmiento/wicket/utils/SiNoDropDownChoice.java
+++ src/main/java/ar/edu/unq/sarmiento/wicket/utils/SiNoDropDownChoice.java
@@ -7,9 +7,6 @@ import org.apache.wicket.markup.html.form.DropDownChoice;
 import org.apache.wicket.model.IModel;
 
 public class SiNoDropDownChoice extends DropDownChoice<Boolean> {
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = -9165991864318804365L;
 	
 	public SiNoDropDownChoice(String id, IModel<Boolean> model) {
