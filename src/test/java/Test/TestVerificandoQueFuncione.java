aecb8df571111a61da986ba4fb6e6c8f42e83c6f
Juan Cruz Santamaria <jcruzsantamaria@hotmail.com>
2018-12-20 20:20:17 -0300

    test agregado

--- src/test/java/Test/TestVerificandoQueFuncione.java
+++ src/test/java/Test/TestVerificandoQueFuncione.java
@@ -3,11 +3,21 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import ar.edu.unq.sarmiento.modelo.EstadoCursada;
+import ar.edu.unq.sarmiento.modelo.EstudioCursado;
+import ar.edu.unq.sarmiento.modelo.TipoDeExamen;
+import ar.edu.unq.sarmiento.wicket.utils.EnumUtils;
+
 public class TestVerificandoQueFuncione {
 
 	@Test
-	public void test() {
-	String prueba ="Matriz";
+	public void testDeEnumUtils() {
+		
+		String estadoDeCursada = EnumUtils.convertirString(EstadoCursada.APROBADA_FINAL);
+		String tipoExamen = EnumUtils.convertirString(TipoDeExamen.PARCIAL);
+		
+		assertTrue(tipoExamen.equals("Parcial"));
+		assertTrue(estadoDeCursada.equals("Aprobada final"));
 	}
 
 }
