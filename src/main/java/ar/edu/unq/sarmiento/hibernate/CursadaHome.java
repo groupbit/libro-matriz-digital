7d9dad0daee60015325d7cd00925d23da08d37c7
Juan Cruz Santamaria <jcruzsantamaria@hotmail.com>
2018-12-20 15:21:13 -0300

    correccion en la forma de busqueda de cursadas y como se muestran el estado

--- src/main/java/ar/edu/unq/sarmiento/hibernate/CursadaHome.java
+++ src/main/java/ar/edu/unq/sarmiento/hibernate/CursadaHome.java
@@ -1,8 +1,5 @@
 package ar.edu.unq.sarmiento.hibernate;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 import org.springframework.stereotype.Component;
 
 import ar.edu.unq.sarmiento.modelo.Cursada;
@@ -11,9 +8,4 @@ import ar.edu.unq.sarmiento.modelo.Cursada;
 public class CursadaHome extends Home<Cursada>{
 
 	private static final long serialVersionUID = 1L;
-	
-	public List<Cursada> listaDeCursadasOrdenadas(Integer id){
-		return this.getSession().createQuery("From Cursada c where alumno_id =: id", Cursada.class).setParameter("id", id)
-				.stream().sorted((c1, c2) -> c1.getMateria().getNombre().compareTo(c2.getMateria().getNombre())).sorted((c1,c2)-> Integer.compare(c1.getAnio(),c2.getAnio())).collect(Collectors.toList());
-	}
 }
--- src/main/java/ar/edu/unq/sarmiento/wicket/cursada/ListadoDeCursadasController.java
+++ src/main/java/ar/edu/unq/sarmiento/wicket/cursada/ListadoDeCursadasController.java
@@ -3,13 +3,11 @@ package ar.edu.unq.sarmiento.wicket.cursada;
 import java.io.Serializable;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import ar.edu.unq.sarmiento.hibernate.CursadaHome;
 import ar.edu.unq.sarmiento.modelo.Alumno;
 import ar.edu.unq.sarmiento.modelo.Cursada;
 import ar.edu.unq.sarmiento.modelo.EstadoCursada;
@@ -21,8 +19,6 @@ public class ListadoDeCursadasController implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	private Alumno alumno;
-	@Autowired
-	private CursadaHome cursadaHome;
 
 	public Alumno getAlumno() {
 		return alumno;
@@ -35,11 +31,12 @@ public class ListadoDeCursadasController implements Serializable {
 	public String getNombreAlumno(){
 		return this.getAlumno().getNombre();
 	}
-
+	
 	public String convertirString(EstadoCursada estado) {
-		return estado.toString();
+		return estado.toString().toLowerCase().replace("_", " ");
 	}
 	public List<Cursada> getCursadas(){
-		return cursadaHome.listaDeCursadasOrdenadas(this.getAlumno().getId());
+		return alumno.getCursadas();
+		
 	}
 }
