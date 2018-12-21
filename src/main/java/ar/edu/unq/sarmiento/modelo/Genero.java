c34f6e4ed3963b566650417388e03570c5e876b4
nataliaMarzec <natimarzec708@gmail.com>
2018-12-21 20:34:03 -0300

    agregado de clase Enum Genero con constructor

---
+++ src/main/java/ar/edu/unq/sarmiento/modelo/Genero.java
@@ -0,0 +1,21 @@
+package ar.edu.unq.sarmiento.modelo;
+
+public enum Genero {
+	
+	FEMENINO("femenino"),MASCULINO("masculino"),OTROS("otros");
+	
+	private String genero;
+	
+	Genero(String genero){
+		this.setGenero(genero);
+	}
+
+	public String getGenero() {
+		return genero;
+	}
+
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+
+}
