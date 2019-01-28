5a54c9ebd6c36ec1a5d9fad6026a3313fe046f93
Juan Cruz Santamaria <jcruzsantamaria@hotmail.com>
2019-01-28 20:36:14 -0300

    cambios olvidados de comitear

---
+++ src/main/java/ar/edu/unq/sarmiento/wicket/crearCursada/CrearCursadaController.java
@@ -0,0 +1,91 @@
+package ar.edu.unq.sarmiento.wicket.crearCursada;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
+
+import ar.edu.unq.sarmiento.hibernate.AlumnoHome;
+import ar.edu.unq.sarmiento.hibernate.CursadaHome;
+import ar.edu.unq.sarmiento.hibernate.MateriaHome;
+import ar.edu.unq.sarmiento.modelo.Alumno;
+import ar.edu.unq.sarmiento.modelo.Cursada;
+import ar.edu.unq.sarmiento.modelo.EstadoCursada;
+import ar.edu.unq.sarmiento.modelo.Materia;
+
+@Service
+@Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+@Transactional
+public class CrearCursadaController implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Autowired
+	private CursadaHome cursadaHome;
+	
+	@Autowired
+	private AlumnoHome alumnoHome;
+	
+	@Autowired
+	private MateriaHome materiaHome;
+
+	private int anio;
+	
+	private Materia materiaElegida;
+	
+	private EstadoCursada estado;
+	
+	private int notaFinal;
+	
+	public void agregarCursada(Alumno alumno) {
+		Cursada cursada = new Cursada();
+		cursada.setAnio(this.getAnio());
+		cursada.setEstado(this.getEstado());
+		cursada.setMateria(this.getMateriaElegida());
+		cursada.setNotaFinal(this.getNotaFinal());
+		alumno.addCursada(cursada);
+		cursadaHome.saveOrUpdate(cursada);
+		alumnoHome.saveOrUpdate(alumno);
+	}
+
+	public int getAnio() {
+		return anio;
+	}
+
+	public void setAnio(int anio) {
+		this.anio = anio;
+	}
+
+	public Materia getMateriaElegida() {
+		return materiaElegida;
+	}
+
+	public void setMateriaElegida(Materia materia) {
+		this.materiaElegida = materia;
+	}
+
+	public EstadoCursada getEstado() {
+		return estado;
+	}
+
+	public void setEstado(EstadoCursada estado) {
+		this.estado = estado;
+	}
+
+	public int getNotaFinal() {
+		return notaFinal;
+	}
+
+	public void setNotaFinal(int notaFinal) {
+		this.notaFinal = notaFinal;
+	}
+	
+	public List<Materia> getTodasLasMaterias(){
+		return materiaHome.all();
+	}
+}
---
+++ src/main/java/ar/edu/unq/sarmiento/wicket/crearCursada/CrearCursadaPage.java
@@ -0,0 +1,65 @@
+package ar.edu.unq.sarmiento.wicket.crearCursada;
+
+import org.apache.wicket.markup.html.form.ChoiceRenderer;
+import org.apache.wicket.markup.html.form.DropDownChoice;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.markup.html.link.Link;
+import org.apache.wicket.model.PropertyModel;
+import org.apache.wicket.spring.injection.annot.SpringBean;
+
+import ar.edu.unq.sarmiento.modelo.Alumno;
+import ar.edu.unq.sarmiento.wicket.crearCarrera.CrearCarreraController;
+import ar.edu.unq.sarmiento.wicket.cursada.ListadoDeCursadasPage;
+import ar.edu.unq.sarmiento.wicket.layout.LayoutPage;
+
+
+public class CrearCursadaPage extends LayoutPage {
+
+	private static final long serialVersionUID = 1L;
+
+	private Alumno alumno;
+
+	@SpringBean(name = "crearCursadaController")
+	private CrearCursadaController crearCursadaController;
+
+	public CrearCursadaPage(Alumno alumno) {
+		this.alumno = alumno;
+		this.formularioCursada();
+	}
+
+	private void formularioCursada() {
+		Form<CrearCarreraController> nuevaCursada = new Form<CrearCarreraController>("cursada") {
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			protected void onSubmit() {
+				crearCursadaController.agregarCursada(alumno);
+				this.setResponsePage(new ListadoDeCursadasPage(alumno));
+
+			}
+		};
+		
+		nuevaCursada.add(new Link<String>("cancelar") {
+
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public void onClick() {
+				this.setResponsePage(new ListadoDeCursadasPage(alumno));
+
+			}
+
+		});
+
+		nuevaCursada.add(new TextField<>("anio",new PropertyModel<>(this.crearCursadaController,"anio")));
+		nuevaCursada.add(new DropDownChoice<>(
+				"materia",
+				new PropertyModel<>(crearCursadaController, "materiaElegida"),
+				new PropertyModel<>(crearCursadaController, "TodasLasMaterias"),
+				new ChoiceRenderer<>("nombre")));
+		nuevaCursada.add(new TextField<>("notaFinal",new PropertyModel<>(this.crearCursadaController,"notaFinal")));
+		this.add(nuevaCursada);
+
+	}
+}
