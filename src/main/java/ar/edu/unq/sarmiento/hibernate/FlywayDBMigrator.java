8108282dd8ee1121805923bb4fde8e27f928eaf8
Federico Aloi <federico.aloi@gmail.com>
2019-03-20 19:38:37 -0300

    Flyway toma el DataSource de Spring

---
+++ src/main/java/ar/edu/unq/sarmiento/hibernate/FlywayDBMigrator.java
@@ -0,0 +1,21 @@
+package ar.edu.unq.sarmiento.hibernate;
+
+import javax.sql.DataSource;
+
+import org.flywaydb.core.Flyway;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class FlywayDBMigrator {
+
+	public static void main(String[] args) {
+		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
+		ctx.scan("ar.edu.unq.sarmiento", "ar.edu.unq.sarmiento.hibernate");
+		ctx.refresh();
+		
+		DataSource dataSource = (DataSource) ctx.getBean("dataSource");
+		Flyway flyway = Flyway.configure().dataSource(dataSource).load();
+		flyway.migrate();
+		
+		ctx.close();
+	}
+}
