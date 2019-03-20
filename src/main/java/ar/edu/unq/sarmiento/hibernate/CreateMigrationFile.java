91ea525bdfb192ec78975405c5327a90613bc956
Federico Aloi <federico.aloi@gmail.com>
2019-03-20 19:56:48 -0300

    Ejecutable para crear una migración vacía

---
+++ src/main/java/ar/edu/unq/sarmiento/hibernate/CreateMigrationFile.java
@@ -0,0 +1,19 @@
+package ar.edu.unq.sarmiento.hibernate;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class CreateMigrationFile {
+	public static void main(String[] args) {
+		String dateTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss"));
+		String migrationFileName = "V" + dateTime + "__Completar_descripcion.sql";
+		String migrationsFolder = "src/main/resources/db/migration";
+		try {
+			new File(migrationsFolder + "/" + migrationFileName).createNewFile();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
