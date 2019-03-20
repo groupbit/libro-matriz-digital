c0e740c1d60a06ad2bbe4f84887dd44c7abd77ca
Federico Aloi <federico.aloi@gmail.com>
2019-03-20 20:17:59 -0300

    Hack para que Hibernate no valide antes de correr las migraciones

--- src/main/java/ar/edu/unq/sarmiento/hibernate/migrations/RunFlywayDBMigrations.java
+++ src/main/java/ar/edu/unq/sarmiento/hibernate/migrations/RunFlywayDBMigrations.java
@@ -5,9 +5,12 @@ import javax.sql.DataSource;
 import org.flywaydb.core.Flyway;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import ar.edu.unq.sarmiento.hibernate.HibernateConf;
+
 public class RunFlywayDBMigrations {
 
 	public static void main(String[] args) {
+		HibernateConf.modo = "migrate";
 		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
 		ctx.scan("ar.edu.unq.sarmiento", "ar.edu.unq.sarmiento.hibernate");
 		ctx.refresh();
