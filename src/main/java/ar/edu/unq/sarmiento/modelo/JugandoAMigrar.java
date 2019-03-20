8c2c0791cedeb894e83d83900ddb8e89127aeaca
Federico Ferreyra Lombardi <federicoferreyra2@gmail.com>
2019-03-20 20:57:28 -0300

    Juando a migrar funcionando

--- src/main/java/ar/edu/unq/sarmiento/modelo/JugandoAMigrar.java
+++ src/main/java/ar/edu/unq/sarmiento/modelo/JugandoAMigrar.java
@@ -5,7 +5,8 @@ import javax.persistence.Entity;
 @Entity
 public class JugandoAMigrar extends Persistible {
 	private String fedeAloi;
-
+	private String fedeFerreyra;
+	
 	public String getFedeAloi() {
 		return fedeAloi;
 	}
---
+++ src/main/resources/db/migration/V20190320203930__Jugando_a_migrar_FedeF.sql
@@ -0,0 +1,1 @@
+alter table JugandoAMigrar add fedeFerreyra varchar(255);
