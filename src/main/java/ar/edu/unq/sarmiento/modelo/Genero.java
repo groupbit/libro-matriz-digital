2caf3408355f52d2111ae204b7017e496b835403
nataliaMarzec <natimarzec708@gmail.com>
2019-03-27 19:34:27 -0300

    Eliminacion de constructor en el Enum Genero

--- src/main/java/ar/edu/unq/sarmiento/modelo/Genero.java
+++ src/main/java/ar/edu/unq/sarmiento/modelo/Genero.java
@@ -3,20 +3,6 @@ package ar.edu.unq.sarmiento.modelo;
 
 public enum Genero {
 	
-	FEMENINO("femenino"),MASCULINO("masculino"),OTROS("otros");
-	
-	private String genero;
-	
-	Genero(String genero){
-		this.setGenero(genero);
-	}
-
-	public String getGenero() {
-		return genero;
-	}
-
-	public void setGenero(String genero) {
-		this.genero = genero;
-	}
+	FEMENINO, MASCULINO,OTROS;
 
 }
