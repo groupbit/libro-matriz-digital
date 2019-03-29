2fb4f7d947e721bc608d3cd1f31b08b9507051b1
Federico Aloi <federico.aloi@gmail.com>
2019-03-29 13:57:58 -0300

    Se captura ModelException y se muestra un error

--- src/main/java/ar/edu/unq/sarmiento/wicket/home/WicketApplication.java
+++ src/main/java/ar/edu/unq/sarmiento/wicket/home/WicketApplication.java
@@ -1,22 +1,46 @@
 package ar.edu.unq.sarmiento.wicket.home;
 
 import org.apache.wicket.Page;
+import org.apache.wicket.core.request.handler.IPageRequestHandler;
+import org.apache.wicket.core.request.handler.PageProvider;
+import org.apache.wicket.core.request.handler.RenderPageRequestHandler;
+import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.protocol.http.WebApplication;
+import org.apache.wicket.request.IRequestHandler;
+import org.apache.wicket.request.cycle.IRequestCycleListener;
+import org.apache.wicket.request.cycle.PageRequestHandlerTracker;
+import org.apache.wicket.request.cycle.RequestCycle;
 import org.apache.wicket.spring.injection.annot.SpringComponentInjector;
 
 import ar.edu.unq.sarmiento.hibernate.HibernateConf;
 import ar.edu.unq.sarmiento.hibernate.HibernateConf.HibernateMode;
+import ar.edu.unq.sarmiento.modelo.ModelException;
 
-public class WicketApplication extends WebApplication{
+public class WicketApplication extends WebApplication {
 
 	@Override
 	public Class<? extends Page> getHomePage() {
 		HibernateConf.modo = HibernateMode.SERVER;
 		return HomePage.class;
 	}
+
 	@Override
 	protected void init() {
 		super.init();
-	    getComponentInstantiationListeners().add(new SpringComponentInjector(this));
+		getComponentInstantiationListeners().add(new SpringComponentInjector(this));
+		getRequestCycleListeners().add(new PageRequestHandlerTracker());
+		getRequestCycleListeners().add(new IRequestCycleListener() {
+			@Override
+			public IRequestHandler onException(RequestCycle cycle, Exception ex) {
+				if (ex instanceof ModelException) {
+					IPageRequestHandler last = PageRequestHandlerTracker.getLastHandler(cycle);
+					WebPage page = (WebPage) (last.getPage());
+					page.error(ex.getMessage());
+					return new RenderPageRequestHandler(new PageProvider(page));
+				} else {
+					return cycle.getActiveRequestHandler();
+				}
+			}
+		});
 	}
 }
--- src/main/java/ar/edu/unq/sarmiento/wicket/layout/LayoutPage.java
+++ src/main/java/ar/edu/unq/sarmiento/wicket/layout/LayoutPage.java
@@ -4,6 +4,7 @@ import org.apache.wicket.Page;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.image.Image;
 import org.apache.wicket.markup.html.link.Link;
+import org.apache.wicket.markup.html.panel.FeedbackPanel;
 import org.apache.wicket.request.resource.PackageResourceReference;
 
 import ar.edu.unq.sarmiento.wicket.alumno.ListadoAlumnoPage;
@@ -15,6 +16,8 @@ public abstract class LayoutPage extends WebPage {
 		this.agregarLink("homePage", HomePage.class);
 		this.agregarLink("alumnosPage", ListadoAlumnoPage.class);
 		this.agregarLink("carrerasPage", ListadoDeCarrerasPage.class);
+		
+		add(new FeedbackPanel("feedback"));
 	}
 	
 	private void agregarLink(String nombre, Class<? extends Page> pageClass) {
