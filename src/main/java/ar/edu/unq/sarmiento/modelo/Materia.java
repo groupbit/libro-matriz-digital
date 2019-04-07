d57192a652075a5d175373733b6259006f3e9b5b
Federico Ferreyra Lombardi <federicoferreyra2@gmail.com>
2019-04-07 19:55:56 -0300

    Validación de no eliminar una materia que ya tiene una cursada de ella

--- src/main/java/ar/edu/unq/sarmiento/modelo/Materia.java
+++ src/main/java/ar/edu/unq/sarmiento/modelo/Materia.java
@@ -2,6 +2,7 @@ package ar.edu.unq.sarmiento.modelo;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
@@ -84,6 +85,10 @@ public class Materia extends Persistible {
 		if(materia.getCorrelativas().contains(this)){
 			throw new ModelException("No puedo agregar "+ this.getNombre() + " porque ya pertenece a las correlativas de " + materia.getNombre());
 		}
+	}
+
+	public void eliminarCorrelatividades(Carrera carrera) {
+		carrera.getListadoMaterias().stream().forEach(m -> m.getCorrelativas().remove(this));
 	}
 	
 
