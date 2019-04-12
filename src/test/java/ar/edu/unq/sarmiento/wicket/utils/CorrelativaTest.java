c7978348a126340329d7468516985bdd813faedf
Federico Ferreyra Lombardi <federicoferreyra2@gmail.com>
2019-04-12 20:46:50 -0300

    Se agrego los test alumnoCursaMateriaConCorrelativa() y alumnoNoPuedeCursarMateriaConCorrelativa()

--- src/test/java/ar/edu/unq/sarmiento/wicket/utils/CorrelativaTest.java
+++ src/test/java/ar/edu/unq/sarmiento/wicket/utils/CorrelativaTest.java
@@ -5,32 +5,74 @@ import static org.junit.Assert.*;
 import java.util.Arrays;
 import java.util.HashSet;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import ar.edu.unq.sarmiento.modelo.Alumno;
 import ar.edu.unq.sarmiento.modelo.Carrera;
+import ar.edu.unq.sarmiento.modelo.Cursada;
+import ar.edu.unq.sarmiento.modelo.EstadoCursada;
 import ar.edu.unq.sarmiento.modelo.Materia;
+import ar.edu.unq.sarmiento.modelo.ModelException;
 
 public class CorrelativaTest {
+	
+	private Materia mate;
+	private Materia intro;
+	private Materia obj;
+	private Alumno pepe;
+
+	@Before
+	public void setup(){
+		mate = new Materia();
+		mate.setNombre("matematica 1");
+		
+		intro = new Materia ();
+		intro.setNombre("introduccion a programacion");
+		
+		obj = new Materia ();
+		obj.setNombre("Objetos 1");
+		
+		pepe = new Alumno();
+		
+		obj.addCorrelativa(mate);
+		
+	}
 	
 	@Test
 	public void agregarCorrelativaandaTest(){
 		Carrera prog = new Carrera();
-		
-		Materia mate = new Materia();
-		mate.setNombre("matematica 1");
-		Materia intro = new Materia ();
-		intro.setNombre("introduccion a programacion");
-		Materia obj = new Materia ();
-		obj.setNombre("Objetos 1");
 	
 		prog.agregarMateria(mate);
 		prog.agregarMateria(obj);
 		prog.agregarMateria(intro);
-		
 		obj.addCorrelativa(intro);
-		obj.addCorrelativa(mate);
+
 		
 		assertEquals(new HashSet<>(Arrays.asList(intro, mate)).stream().count(), obj.getCorrelativas().size());
-		}	
+	}
+	
+	@Test
+	public void alumnoCursaMateriaConCorrelativa(){
+		this.agregarCursadas(EstadoCursada.APROBADA_FINAL);
+		
+	}
+	
+	@Test(expected = ModelException.class)
+	public void alumnoNoPuedeCursarMateriaConCorrelativa(){
+		this.agregarCursadas(EstadoCursada.CURSANDO);
+		
+	}
+
+	private void agregarCursadas(EstadoCursada estado) {
+		Cursada cursadaMate = new Cursada();
+		cursadaMate.setMateria(mate);
+		cursadaMate.setEstado(estado);
+		pepe.addCursada(cursadaMate);
+		
+		Cursada cursadaProg = new Cursada();
+		cursadaProg.setMateria(obj);
+		pepe.addCursada(cursadaProg);
+	}
 	
 }
