753a84a0c745546ce4584626ffcdd1e5231e667d
Federico Aloi <federico.aloi@gmail.com>
2019-06-12 21:02:37 -0300

    Página de login más bonita y cerrar sesión

--- src/main/java/ar/edu/unq/sarmiento/wicket/auth/BasicAuthenticationSession.java
+++ src/main/java/ar/edu/unq/sarmiento/wicket/auth/BasicAuthenticationSession.java
@@ -4,14 +4,21 @@ import org.apache.wicket.authroles.authentication.AuthenticatedWebSession;
 import org.apache.wicket.authroles.authorization.strategies.role.Roles;
 import org.apache.wicket.request.Request;
 
+import ar.edu.unq.sarmiento.utils.Env;
+
 public class BasicAuthenticationSession extends AuthenticatedWebSession {
+	private String adminUser;
+	private String adminPassword;
+
 	public BasicAuthenticationSession(Request request) {
 		super(request);		
+		this.adminUser = Env.getOrElse("ADMIN_USER", "admin");
+		this.adminPassword = Env.getOrElse("ADMIN_PASSWORD", "admin");
 	}
 
 	@Override
 	public boolean authenticate(String username, String password) {
-		return username.equals("admin") && password.equals("admin1234");
+		return username.equals(this.adminUser) && password.equals(this.adminPassword);
 	}
 
 	@Override
